59ec1f2f9d99282008023963a683d820e5ada923
ihym <b.diakomanolis@gmail.com>
2013-07-06 19:40:38 +0300

    Version 1

--- Libraries/Navigation/Navigation.c
+++ Libraries/Navigation/Navigation.c
@@ -15,18 +15,18 @@ void move_backwards(int speedpinA,int speedpinB,int pinI1A,int pinI2A,int pinI1B
     digitalWrite(pinI2B,LOW);
     analogWrite(speedpinA,50);//input a value to set the speed
     analogWrite(speedpinB,50);
-    delay(2000);
+    delay(1000);
 }
 
 void turn_left(int speedpinA,int speedpinB,int pinI1A,int pinI2A,int pinI1B,int pinI2B){
-    digitalWrite(pinI1A,LOW);// DC motor A rotates clockwise
+    digitalWrite(pinI1A,LOW); // DC motor A rotates clockwise
     digitalWrite(pinI2A,HIGH);
-    digitalWrite(pinI1B,LOW);// DC motor B rotates clockwise
+    digitalWrite(pinI1B,LOW); // DC motor B rotates clockwise
     digitalWrite(pinI2B,HIGH);
-    analogWrite(speedpinA,100);//input different speed values to set the curvature
-    analogWrite(speedpinB,150);
+    analogWrite(speedpinA,98); // different speed values to set the desired angle/if 255->234
+    analogWrite(speedpinB,100);
 
-    delay(2000);
+    delay(200);
 
     analogWrite(speedpinA,100);
     analogWrite(speedpinB,100);
@@ -35,14 +35,14 @@ void turn_left(int speedpinA,int speedpinB,int pinI1A,int pinI2A,int pinI1B,int 
 }
 
 void turn_right(int speedpinA,int speedpinB,int pinI1A,int pinI2A,int pinI1B,int pinI2B){
-    digitalWrite(pinI1A,LOW);// DC motor A rotates clockwise
+    digitalWrite(pinI1A,LOW); // DC motor A rotates clockwise
     digitalWrite(pinI2A,HIGH);
-    digitalWrite(pinI1B,LOW);// DC motor B rotates clockwise
+    digitalWrite(pinI1B,LOW); // DC motor B rotates clockwise
     digitalWrite(pinI2B,HIGH);
-    analogWrite(speedpinA,150);//input different speed values to set the curvature
-    analogWrite(speedpinB,100);
+    analogWrite(speedpinA,100); // different speed values to set the desired angle/if 255->234
+    analogWrite(speedpinB,98);
 
-    delay(2000);
+    delay(200);
 
     analogWrite(speedpinA,100);
     analogWrite(speedpinB,100);
@@ -56,5 +56,5 @@ void stop_rotating(int pinI1A,int pinI2A,int pinI1B,int pinI2B){
     digitalWrite(pinI1B,HIGH);// DC motor B stop rotating
     digitalWrite(pinI2B,HIGH);
 
-    delay(500);
+    delay(5000);
 }
--- control/control.ino
+++ control/control.ino
@@ -1,27 +1,32 @@
 #include <SoftwareSerial.h>
-#include <Move.h>
+#include <Navigation.h>
 #include <TinyGPS.h>
-// TO DO X Y Z ACCELARATIONS !!!!!!!!!!!!!!!!!!!!!!!!!
+
+#define TARGET_LAT 38.24
+#define TARGET_LON 23
+#define MIN_DIST 10
+#define TIME_CTRL 1000
+
 TinyGPS gps;
 SoftwareSerial nss(3, 4);
-static void plsr(char c);//Returns the angle in degrees from north
-static void gpsdump();//Returns GPS data
-static bool feedgps();
-float ftarget_angle;
-float ftarget_course;
-int pinI1A=6;//define I1 port DC motor A
-int pinI2A=7;//define I2 port DC motor A
-int pinI1B=8;//define I1 port DC motor B
-int pinI2B=9;//define I2 port DC motor B
-int speedpinA=10;//define EA(PWM speed regulation)port
-int speedpinB=11;//define EB(PWM speed regulation)port
+void XYZ_accelerations(char); //Returns the angle in degrees from north
+bool feedgps();
+float controller(float,float);
+
+
+int pinI1A=6; //define I1 port DC motor A
+int pinI2A=7; //define I2 port DC motor A
+int pinI1B=8; //define I1 port DC motor B
+int pinI2B=9; //define I2 port DC motor B
+int speedpinA=10; //define EA(PWM speed regulation)port
+int speedpinB=11; //define EB(PWM speed regulation)port
 
 void setup(){
-    pinMode(pinI1A,OUTPUT);//define this port as output
+    pinMode(pinI1A,OUTPUT); //define this port as output
     pinMode(pinI2A,OUTPUT);
     pinMode(speedpinA,OUTPUT);
 
-    pinMode(pinI1B,OUTPUT);//define this port as output
+    pinMode(pinI1B,OUTPUT); //define this port as output
     pinMode(pinI2B,OUTPUT);
     pinMode(speedpinB,OUTPUT);
 
@@ -30,192 +35,138 @@ void setup(){
 }
 
 void loop(){
-  bool newdata = false;
-  unsigned long start = millis();
-  
-  // Every second we print an update
-  while (millis() - start < 1000)
-  {
-    if (feedgps()){
-      gpsdump();
-    }
-    else;//STOP AND WAIT.
-  }
+	unsigned long start = millis();
+	long age;
+    float flat,flon,faltitude,fcourse,fspeed,fdistance,ftarget_course;
+    unsigned long start = millis();
+    // ******************* loop timing control *******************
+    while (millis() - start < TIME_CTRL){
+    	// ********************* Sensor Acquisition *******************
+		if (feedgps()){
+			gps.f_get_position(&flat,&flon,&age);
+			faltitude = gps.f_altitude();
+			fcourse = gps.f_course();
+			ftarget_course = TinyGPS::course_to(flat, flon, TARGET_LAT, TARGET_LON);
+			fdistance = TinyGPS::distance_between(flat, flon, TARGET_LAT , TARGET_LON);
+			// ******************* Angle Control & Motor Drive & Debugging Info *******************
+			if (fdistance<MIN_DIST){
+				stop_rotating(pinI1A,pinI2A,pinI1B,pinI2B);
+				Serial.print("Stop Rotating");
+			}
+			else{
+				if(controller(fcourse,ftarget_course)>20 && controller(fcourse,ftarget_course)<=180){
+					turn_right(speedpinA,speedpinB,pinI1A,pinI2A,pinI1B,pinI2B);
+					Serial.print("Turn Left");
+				}
+				else if(controller(fcourse,ftarget_course)>180 && controller(fcourse,ftarget_course)<340){
+					turn_left(speedpinA,speedpinB,pinI1A,pinI2A,pinI1B,pinI2B);
+					Serial.print("Turn Right");
+				}
+				else{
+					move_fowards(speedpinA,speedpinB,pinI1A,pinI2A,pinI1B,pinI2B);
+					Serial.print("Move Forwards");
+				}
+			}
+		}
+	}
 }
 
-static void gpsdump(){
-  float flat, flon,faltitude,fcourse,fspeed,fdistance;
-  unsigned long age, date, time, chars = 0;
-  unsigned short sentences = 0, failed = 0;
-  static const float TARGET_LAT = 38.24, TARGET_LON = 23;
-  
-  gps.f_get_position(&flat, &flon, &age);
-  faltitude = gps.f_altitude();
-  fcourse = gps.f_course();
-  fspeed = gps.f_speed_kmph()/3.6;
-  fdistance = TinyGPS::distance_between(flat, flon, TARGET_LAT , TARGET_LON);
-  ftarget_course = TinyGPS::course_to(flat, flon, TARGET_LAT, TARGET_LON);
-  
-  Serial.print("LAT:  ");
-  Serial.println(flat);
-  Serial.print(" LON: ");
-  Serial.println(flon);
-  Serial.println(faltitude);
-  Serial.println(fcourse);
-  Serial.println(fspeed);
-  Serial.println(fdistance);
-  Serial.println(ftarget_course);
+bool feedgps(){
+	while (nss.available()){
+		char c = nss.read(); 
+		if (gps.encode(c))
+			return true;
+	}
+	return false;
 }
 
-static bool feedgps()
-{
-  while (nss.available())
-  {
-    char c = nss.read(); 
-    if (gps.encode(c))
-      return true;
-    else
-      plsr(c);
-  }
-  
-  return false;
+float controller(float course,float target_course){
+	if (course-target_course<0)
+		return 360+course-target_course;
+	else 
+		return course-target_course;
 }
 
-static void plsr(char c){
-  static int i = 0;
-  static int ia = 0;
-  static int ib = 0;
-  static int ic = 0;
-  static int magnet_course =0;
-  static int X = 0;
-  static int Y = 0;
-  static int Z = 0;
-  static int J = 0;
-  static int K = 0;
-  static int comma_count = 0;
-  char *sentence="$PLSR,245,1,";
+/*
+void XYZ_accelerations(char c){
+	static int i = 0;
+	static int ia = 0;
+	static int ib = 0;
+	static int ic = 0;
+	static int X = 0;
+	static int Y = 0;
+	static int Z = 0;
+	static int comma_count = 0;
+	char *sentence="$PLSR,245,1,";
   
-  if(i>11){
-    
-    if(c == ',')
-    {
-      comma_count++;
-    }
-    
-    if(comma_count == 0)
-    {
-      if(i == 12)
-      {
-        magnet_course = ((int)c-((int)'0'));
-      }
-      if(i == 13)
-      {
-        int j = magnet_course;
-        magnet_course = 10*j+((int)c-((int)'0'));
-      }
-      if(i == 14)
-      {
-        int k = magnet_course/10;
-        int l = magnet_course%10;
-        magnet_course = 100*k+10*l+((int)c-((int)'0'));
-      }
-    }
-    
-    if(comma_count == 2)
-    {
-      if(ia == 0)
-      {
-        ia = i + 1;
-      }
-      
-      if(i == ia)
-      {
-        X = ((int)c-((int)'0'));
-      }
-      if(i == ia + 1)
-      {
-        int j = X;
-        X = 10*j+((int)c-((int)'0'));
-      }
-      if(i == ia + 2)
-      {
-        int k = X/10;
-        int l = X%10;
-        X = 100*k+10*l+((int)c-((int)'0'));
-      }
-    }
-    
-    if(comma_count == 3)
-    {
-      if(ib == 0)
-      {
-        ib = i + 1;
-      }
-      
-      if(i == ib)
-      {
-        Y = ((int)c-((int)'0'));
-      }
-      if(i == ib + 1)
-      {
-        int j = Y;
-        Y = 10*j+((int)c-((int)'0'));
-      }
-      if(i == ib + 2)
-      {
-        int k = Y/10;
-        int l = Y%10;
-        Y = 100*k+10*l+((int)c-((int)'0'));
-      }
-    }
-    
-    if(comma_count == 4)
-    {
-      if(ic == 0)
-      {
-        ic = i + 1;
-      }
-      
-      if(i == ic)
-      {
-        Z = ((int)c-((int)'0'));
-      }
-      if(i == ic + 1)
-      {
-        int j = Z;
-        Z = 10*j+((int)c-((int)'0'));
-      }
-      if(i == ic + 2)
-      {
-        int k = Z/10;
-        int l = Z%10;
-        Z = 100*k+10*l+((int)c-((int)'0'));
-      }
-    }
-    
-    i++;
-    
-    if(c == ',' && comma_count > 4)
-    {
-      Serial.println("--------------------");
-      Serial.println(magnet_course);
-      Serial.print("X: ");
-      Serial.println(X*(2.0/512.0));
-      Serial.print("Y: ");
-      Serial.println(Y*(2.0/512.0));
-      Serial.print("Z: ");
-      Serial.println(Z*(2.0/512.0));
-      ftarget_angle = ftarget_course - magnet_course;
-      i=0;
-      comma_count = 0;
-      return;
-    }
-  }
-
-  else{
-    magnet_course=0;
-    if(sentence[i]==c){i++;}
-    else i=0;
-  }
-  
+	if(i>11){
+		if(c == ',')
+			comma_count++;
+		if(comma_count == 2){
+			if(ia == 0)
+				ia = i + 1;
+			if(i == ia)
+				X = ((int)c-((int)'0'));
+			if(i == ia + 1){
+				int j = X;
+				X = 10*j+((int)c-((int)'0'));
+			}
+			if(i == ia + 2){
+				int k = X/10;
+				int l = X%10;
+				X = 100*k+10*l+((int)c-((int)'0'));
+			}
+		}	
+		if(comma_count == 3){
+			if(ib == 0)
+				ib = i + 1;	
+			if(i == ib)
+				Y = ((int)c-((int)'0'));
+			if(i == ib + 1){
+				int j = Y;
+				Y = 10*j+((int)c-((int)'0'));
+			}
+			if(i == ib + 2){
+				int k = Y/10;
+				int l = Y%10;
+				Y = 100*k+10*l+((int)c-((int)'0'));
+			}
+		}
+		if(comma_count == 4){
+			if(ic == 0)
+				ic = i + 1;
+			if(i == ic)
+				Z = ((int)c-((int)'0'));
+			if(i == ic + 1){
+				int j = Z;
+				Z = 10*j+((int)c-((int)'0'));
+			}
+			if(i == ic + 2){
+				int k = Z/10;
+				int l = Z%10;
+				Z = 100*k+10*l+((int)c-((int)'0'));
+			}
+		}
+		i++;
+		if(c == ',' && comma_count > 4){
+			Serial.println("--------------------");
+			Serial.println(magnet_course);
+			Serial.print("X: ");
+			Serial.println(X*(2.0/512.0));
+			Serial.print("Y: ");
+			Serial.println(Y*(2.0/512.0));
+			Serial.print("Z: ");
+			Serial.println(Z*(2.0/512.0));
+			i=0;
+			comma_count = 0;
+			return;
+		}
+	}
+	else{
+		if(sentence[i]==c)
+			i++;
+		else 
+			i=0;
+	}
 }
+*/
